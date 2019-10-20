b0ab50dce91e903da9b3965d54a1a3348d508a36
askerzade <pedro100500@gmail.com>
2019-10-20 19:50:40 +0400

    --base

--- src/main/java/homework2/homework2.java
+++ src/main/java/homework2/homework2.java
@@ -1,23 +1,89 @@
 package homework2;
 
-import java.util.Scanner;
 import java.util.Random;
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class homework2 {
     public static void main(String[] args) {
         System.out.println("All set. Get ready to rumble!");
-        char[][] a= new char[6][12];
-        for(int x=0; x<=a.length-1 ; x++) {
+        String[][] a = new String[6][6];
+        a[0][0] = "0|";
+        a[0][1] = "1|";
+        a[0][2] = "2|";
+        a[0][3] = "3|";
+        a[0][4] = "4|";
+        a[0][5] = "5|";
+        a[1][0] = "1|";
+        a[2][0] = "2|";
+        a[3][0] = "3|";
+        a[4][0] = "4|";
+        a[5][0] = "5|";
+        int b = a.length;
+        Random random = new Random();
+        int targethorizontal;
+        int targetvertical;
+        int playerhorizontal;
+        int playervertical;
+        boolean isTrue = false;
+        int x;
+        int y;
+        Scanner s1 = new Scanner(System.in);
+        Scanner s2 = new Scanner(System.in);
+        do {
+            targethorizontal = random.nextInt(b);
+            targetvertical = random.nextInt(b);
+        } while (!(targethorizontal != 0 & targetvertical != 0));
+        for (x = 0; x < a.length; x++) {
             System.out.println();
-            for (int y = 0; y<=a[x].length-1 ; y++) {
-                if(y==0) {
-                    System.out.print(x);
+            for (y = 0; y < a[x].length; y++) {
+                if (x == 0 || y == 0) System.out.print(a[x][y]);
+                else System.out.print("-|");
+            }
+        }
+        System.out.println();
+        while (!isTrue) {
+            System.out.println();
+            while (true) {
+                System.out.println("Chose horizontal shooting coordinate");
+                playerhorizontal = s1.nextInt();
+                if (playerhorizontal == 0 || playerhorizontal > 5)
+                    System.out.println("inappropriate coordinate!");
+                else break;
+            }
+            while (true) {
+                System.out.println("Chose vertical shooting coordinate");
+                playervertical = s2.nextInt();
+                if (playervertical == 0 || playervertical > 5)
+                    System.out.println("inappropriate coordinate!");
+                else break;
+            }
+            if (playerhorizontal == targethorizontal && playervertical == targetvertical) {
+                isTrue = true;
+                for (x = 0; x < a.length; x++) {
+                    System.out.println();
+                    for ( y = 0; y < a[x].length; y++) {
+                        if (x == 0 || y == 0) System.out.print(a[x][y]);
+                   //     else if (x == playerhorizontal && y == playervertical)
+                   //         System.out.print("*|");
+                        else if (x == playerhorizontal && y == playervertical)
+                            System.out.print("x|");
+                        else System.out.print("-|");
+                    }
                 }
-                if(y==1||y==3||y==5||y==7||y==9||y==11){
-                    System.out.print("|");
+                System.out.println();
+                System.out.println("Congratulation! You have Won!");
+            } else {
+                for ( x = 0; x < a.length; x++) {
+                    System.out.println();
+                    for (y = 0; y < a[x].length; y++) {
+                        if (x == 0 || y == 0) System.out.print(a[x][y]);
+                        else if (x == playerhorizontal && y == playervertical)
+                            System.out.print("*|");
+                        else System.out.print("-|");
+                    }
+
                 }
-                else System.out.print("-");
+
             }
         }
     }
