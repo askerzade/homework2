a0b364d2ef830580960bb1ffbbe1639d197d0c30
askerzade <pedro100500@gmail.com>
2019-10-23 19:40:39 +0400

    --hw4 2nd family

--- src/main/java/homework3/Scedule.java
+++ src/main/java/homework3/Scedule.java
@@ -5,21 +5,21 @@ import java.util.Scanner;
 public class Scedule {
     public static void main(String[] args) {
         boolean cycleBreak = false;
-        String[][] Scedule = new String[7][2];
-        Scedule[0][0] = "Sunday";
-        Scedule[0][1] = "do home work";
-        Scedule[1][0] = "Monday";
-        Scedule[1][1] = "go to courses; watch a film";
-        Scedule[2][0] = "Tuesday";
-        Scedule[2][1] = "meet with John";
-        Scedule[3][0] = "Wednesday";
-        Scedule[3][1] = "do home cleaning";
-        Scedule[4][0] = "Thursday";
-        Scedule[4][1] = "visit grandma";
-        Scedule[5][0] = "Friday";
-        Scedule[5][1] = "meet with group mates";
-        Scedule[6][0] = "Saturday";
-        Scedule[6][1] = "call to tutor";
+        String[][] Schedule = new String[7][2];
+        Schedule[0][0] = "Sunday";
+        Schedule[0][1] = "do home work";
+        Schedule[1][0] = "Monday";
+        Schedule[1][1] = "go to courses; watch a film";
+        Schedule[2][0] = "Tuesday";
+        Schedule[2][1] = "meet with John";
+        Schedule[3][0] = "Wednesday";
+        Schedule[3][1] = "do home cleaning";
+        Schedule[4][0] = "Thursday";
+        Schedule[4][1] = "visit grandma";
+        Schedule[5][0] = "Friday";
+        Schedule[5][1] = "meet with group mates";
+        Schedule[6][0] = "Saturday";
+        Schedule[6][1] = "call to tutor";
         while (!cycleBreak) {
             System.out.println();
             System.out.println("Please, input the day of the week:");
@@ -28,25 +28,25 @@ public class Scedule {
             String input1 = input.replace(" ", "");
             switch (input1.toLowerCase()) {
                 case "sunday":
-                    System.out.printf("Your tasks for %s: %s.", Scedule[0][0], Scedule[0][1]);
+                    System.out.printf("Your tasks for %s: %s.", Schedule[0][0], Schedule[0][1]);
                     break;
                 case "monday":
-                    System.out.printf("Your tasks for %s: %s.", Scedule[1][0], Scedule[1][1]);
+                    System.out.printf("Your tasks for %s: %s.", Schedule[1][0], Schedule[1][1]);
                     break;
                 case "tuesday":
-                    System.out.printf("Your tasks for %s: %s.", Scedule[2][0], Scedule[2][1]);
+                    System.out.printf("Your tasks for %s: %s.", Schedule[2][0], Schedule[2][1]);
                     break;
                 case "wednesday":
-                    System.out.printf("Your tasks for %s: %s.", Scedule[3][0], Scedule[3][1]);
+                    System.out.printf("Your tasks for %s: %s.", Schedule[3][0], Schedule[3][1]);
                     break;
                 case "thursday":
-                    System.out.printf("Your tasks for %s: %s.", Scedule[4][0], Scedule[4][1]);
+                    System.out.printf("Your tasks for %s: %s.", Schedule[4][0], Schedule[4][1]);
                     break;
                 case "friday":
-                    System.out.printf("Your tasks for %s: %s.", Scedule[5][0], Scedule[5][1]);
+                    System.out.printf("Your tasks for %s: %s.", Schedule[5][0], Schedule[5][1]);
                     break;
                 case "saturday":
-                    System.out.printf("Your tasks for %s: %s.", Scedule[6][0], Scedule[6][1]);
+                    System.out.printf("Your tasks for %s: %s.", Schedule[6][0], Schedule[6][1]);
                     break;
 
                 case "exit":
